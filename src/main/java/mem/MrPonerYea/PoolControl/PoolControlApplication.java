ab8af5ee25982cf8f5ab9f043c9a7f01befaa8b9
MrPonerYea <MrPonerYea.mem@gmail.com>
2021-09-22 11:15:28 +0300

    Добавлена аннотация @EnableJpaAuditing

--- src/main/java/mem/MrPonerYea/PoolControl/PoolControlApplication.java
+++ src/main/java/mem/MrPonerYea/PoolControl/PoolControlApplication.java
@@ -2,7 +2,9 @@ package mem.MrPonerYea.PoolControl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PoolControlApplication {
 
