fe160e2b6cee94ad30b69d60e873ff1c3b63d979
MrPonerYea <MrPonerYea.mem@gmail.com>
2021-09-24 13:45:28 +0300

    Изменен способ автоматической генерации id в таблицах

--- src/main/java/mem/MrPonerYea/PoolControl/model/entity/BaseEntity.java
+++ src/main/java/mem/MrPonerYea/PoolControl/model/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -13,14 +14,16 @@ import java.util.Date;
 @Data
 public abstract class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @CreatedDate
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "created_at", updatable = false)
     private Date createdAt;
 
     @LastModifiedDate
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "updated_at")
     private Date updatedAt;
 }
