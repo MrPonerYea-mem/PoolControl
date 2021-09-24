1cad635c4089b371014e882a3633f2d612b37cdf
MrPonerYea <MrPonerYea.mem@gmail.com>
2021-09-24 13:46:12 +0300

    Доработан скрипт для генерации значений

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,50 +21,50 @@ CREATE TABLE GROUPS (
 );
 */
 -- INSTRUCTORS
-INSERT INTO users (id, username, password, gender, role, time_to_work, prefer_time_start, prefer_time_end,
+INSERT INTO users (id, username, password, email, gender, role, time_to_work, prefer_time_start, prefer_time_end,
                    created_at, updated_at)
-VALUES (1, 'instructor1', 'instructor1', 'MALE', 'INSTRUCTOR', 40, 32400, 43200,
+VALUES (1, 'instructor1', 'instructor1', 'user4@mail.com', 'MALE', 'INSTRUCTOR', 40, 32400, 43200,
         '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
 
-INSERT INTO users (id, username, password, gender, role, time_to_work, prefer_time_start, prefer_time_end,
+INSERT INTO users (id, username, password, email, gender, role, time_to_work, prefer_time_start, prefer_time_end,
                    created_at, updated_at)
-VALUES (2, 'instructor2', 'instructor2', 'MALE', 'INSTRUCTOR', 1, 43200, 50400,
+VALUES (2, 'instructor2', 'instructor2', 'user4@mail.com', 'MALE', 'INSTRUCTOR', 1, 43200, 50400,
         '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
 
-INSERT INTO users (id, username, password, gender, role, time_to_work, prefer_time_start, prefer_time_end,
+INSERT INTO users (id, username, password, email, gender, role, time_to_work, prefer_time_start, prefer_time_end,
                    created_at, updated_at)
-VALUES (3, 'instructor3', 'instructor3', 'MALE', 'INSTRUCTOR', 10, 32400, 43200,
+VALUES (3, 'instructor3', 'instructor3', 'user4@mail.com', 'MALE', 'INSTRUCTOR', 10, 32400, 43200,
         '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
 
 -- USERS
-INSERT INTO users (id, username, password, gender, role, created_at, updated_at)
-VALUES (4, 'user4', 'user4', 'MALE', 'USER', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
+INSERT INTO users (id, username, password, email, gender, role, created_at, updated_at)
+VALUES (4, 'user4', 'user4', 'user4@mail.com', 'MALE', 'USER', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
 
-INSERT INTO users (id, username, password, gender, role, created_at, updated_at)
-VALUES (5, 'user5', 'user5', 'MALE', 'USER', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
+INSERT INTO users (id, username, password, email, gender, role, created_at, updated_at)
+VALUES (5, 'user5', 'user5', 'user4@mail.com', 'MALE', 'USER', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
 
-INSERT INTO users (id, username, password, gender, role, created_at, updated_at)
-VALUES (6, 'user6', 'user6', 'MALE', 'USER', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
+INSERT INTO users (id, username, password, email, gender, role, created_at, updated_at)
+VALUES (6, 'user6', 'user6', 'user4@mail.com', 'MALE', 'USER', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
 
 -- GROUPS
 INSERT INTO groups (id, time_start, cloakroom_m, cloakroom_w, user_id, created_at, updated_at)
-VALUES (1, '0000-00-00 14:00:00.000', 1, 2, 1, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+VALUES (1, 43200, 1, 2, 1, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
 
 INSERT INTO groups (id, time_start, cloakroom_m, cloakroom_w, user_id, created_at, updated_at)
-VALUES (2, '0000-00-00 14:00:00.000', 10, 20, 2, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+VALUES (2, 43200, 10, 20, 2, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
 
 INSERT INTO groups (id, time_start, cloakroom_m, cloakroom_w, user_id, created_at, updated_at)
-VALUES (3, '0000-00-00 14:00:00.000', 0, 2, 3, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+VALUES (3, 43200, 0, 2, 3, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
 
 --GROUPS-USER
 INSERT INTO users_groups (id, user_id, group_id, created_at, updated_at)
 VALUES (1, 4, 1, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
 
 INSERT INTO users_groups (id, user_id, group_id, created_at, updated_at)
-VALUES (1, 4, 2, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+VALUES (2, 4, 2, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
 
 INSERT INTO users_groups (id, user_id, group_id, created_at, updated_at)
-VALUES (2, 5, 1, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+VALUES (3, 5, 1, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
 
 INSERT INTO users_groups (id, user_id, group_id, created_at, updated_at)
-VALUES (3, 6, 2, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+VALUES (4, 6, 2, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
