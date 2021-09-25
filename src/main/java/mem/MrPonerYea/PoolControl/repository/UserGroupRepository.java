c7d46bb50697600764b369aa56a1efe1eeb244a4
MrPonerYea <MrPonerYea.mem@gmail.com>
2021-09-25 21:51:57 +0300

    Добавлены методы для подсчета кол-ва людей в группе

--- src/main/java/mem/MrPonerYea/PoolControl/repository/UserGroupRepository.java
+++ src/main/java/mem/MrPonerYea/PoolControl/repository/UserGroupRepository.java
@@ -1,9 +1,18 @@
 package mem.MrPonerYea.PoolControl.repository;
 
 import mem.MrPonerYea.PoolControl.model.entity.user.UserGroupEntity;
+import mem.MrPonerYea.PoolControl.model.enumeration.GenderEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserGroupRepository extends JpaRepository<UserGroupEntity, Long> {
+
+    @Query("select count(ug.user.id) from UserGroupEntity ug where ug.group.id = :groupId")
+    Integer getUsersCountInGroup(@Param("groupId") Long groupId);
+
+    @Query("select count(ug.user.id) from UserGroupEntity ug where ug.group.id = :groupId and ug.user.gender = :gender")
+    Integer getUsersCountInGroupByGender(@Param("groupId") Long groupId, @Param("gender")GenderEnum gender);
 }
