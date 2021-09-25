3eaf0eb2bb57bcd8cda716c64b40a9b7c996e807
MrPonerYea <MrPonerYea.mem@gmail.com>
2021-09-25 21:42:17 +0300

    Обновлен скрипт с созданием данных

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -44,7 +44,11 @@ INSERT INTO users (id, username, password, email, gender, role, created_at, upda
 VALUES (5, 'user5', 'user5', 'user4@mail.com', 'MALE', 'USER', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
 
 INSERT INTO users (id, username, password, email, gender, role, created_at, updated_at)
-VALUES (6, 'user6', 'user6', 'user4@mail.com', 'MALE', 'USER', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
+VALUES (6, 'user6', 'user6', 'user4@mail.com', 'FEMALE', 'USER', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
+
+-- ADMIN
+INSERT INTO users (id, username, password, email, gender, role, created_at, updated_at)
+VALUES (7, 'a', 'a', 'user4@mail.com', 'FEMALE', 'ADMIN', '2021-08-22 15:58:49.713', '2021-08-22 15:58:49.713');
 
 -- GROUPS
 INSERT INTO groups (id, time_start, cloakroom_m, cloakroom_w, user_id, created_at, updated_at)
@@ -54,7 +58,10 @@ INSERT INTO groups (id, time_start, cloakroom_m, cloakroom_w, user_id, created_a
 VALUES (2, 43200, 10, 20, 2, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
 
 INSERT INTO groups (id, time_start, cloakroom_m, cloakroom_w, user_id, created_at, updated_at)
-VALUES (3, 43200, 0, 2, 3, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+VALUES (3, 43200, 1, 2, 3, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+
+INSERT INTO groups (id, time_start, cloakroom_m, cloakroom_w, user_id, created_at, updated_at)
+VALUES (4, 43200, 1, 0, 3, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
 
 --GROUPS-USER
 INSERT INTO users_groups (id, user_id, group_id, created_at, updated_at)
@@ -68,3 +75,12 @@ VALUES (3, 5, 1, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
 
 INSERT INTO users_groups (id, user_id, group_id, created_at, updated_at)
 VALUES (4, 6, 2, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+
+INSERT INTO users_groups (id, user_id, group_id, created_at, updated_at)
+VALUES (5, 6, 3, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+
+INSERT INTO users_groups (id, user_id, group_id, created_at, updated_at)
+VALUES (6, 7, 3, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
+
+INSERT INTO users_groups (id, user_id, group_id, created_at, updated_at)
+VALUES (7, 4, 4, '2021-09-19 15:58:49.713', '2021-09-19 15:58:49.713');
