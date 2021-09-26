bea3f3151988d44e70b73d5a961a7f5539e2c9de
MrPonerYea <MrPonerYea.mem@gmail.com>
2021-09-26 12:38:37 +0300

    Добавлены тесты для UserController

--- src/test/java/mem/MrPonerYea/PoolControl/UserControllerTest.java
+++ src/test/java/mem/MrPonerYea/PoolControl/UserControllerTest.java
@@ -1,47 +1,56 @@
 package mem.MrPonerYea.PoolControl;
 
-import mem.MrPonerYea.PoolControl.controller.user.UserController;
+import mem.MrPonerYea.PoolControl.controller.group.GroupController;
 import mem.MrPonerYea.PoolControl.model.dto.UserRequestDto;
 import mem.MrPonerYea.PoolControl.model.enumeration.GenderEnum;
 import mem.MrPonerYea.PoolControl.model.enumeration.RoleEnum;
-import mem.MrPonerYea.PoolControl.repository.user.UserRepository;
+import mem.MrPonerYea.PoolControl.repository.UserGroupRepository;
+import mem.MrPonerYea.PoolControl.repository.group.GroupRepository;
+import mem.MrPonerYea.PoolControl.service.group.GroupService;
 import mem.MrPonerYea.PoolControl.service.user.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.web.server.LocalServerPort;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@Import(value = UserController.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// Для работы теста нужно удалить @EnableJpaAuditing из главного класса, костыль)
 @RunWith(SpringRunner.class)
+@WebMvcTest(GroupController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class UserControllerTest {
     @Autowired
-    private UserController userController;
+    private MockMvc mockMvc;
 
+    @Autowired
+    private GroupController groupController;
+
+    @MockBean
+    private GroupService groupService;
+
+    @MockBean
+    private GroupRepository groupRepository;
     @MockBean
     private UserService userService;
-
     @MockBean
-    private UserRepository userRepository;
+    private UserGroupRepository userGroupRepository;
 
     @Test
-    public void testCreateUser() {
-        UserRequestDto user = createUser("user_test_second", "user321", GenderEnum.MALE, RoleEnum.USER,
-                42300, 63200, 12);
-
+    public void testCreateUserGet() throws Exception {
+        mockMvc.perform(get("/groups/users_groups/1")).andExpect(status().is4xxClientError());
     }
 
-    private UserRequestDto createUser(String userName, String pass, GenderEnum gender, RoleEnum role,
-                                      Integer timeStart, Integer timeEnd, Integer timeWork) {
-        UserRequestDto userRequestDto = new UserRequestDto();
-        userRequestDto.setUsername(userName);
-        userRequestDto.setPassword(pass);
-        userRequestDto.setGender(gender);
-        userRequestDto.setRole(role);
-        userRequestDto.setPreferTimeEnd(timeEnd);
-        userRequestDto.setPreferTimeStart(timeStart);
-        userRequestDto.setTimeToWork(timeWork);
-        return userRequestDto;
+    @Test
+    public void testCreateUserPost() throws Exception {
+        mockMvc.perform(post("/groups/users_groups/admin/1")).andExpect(status().is4xxClientError());
     }
 }
